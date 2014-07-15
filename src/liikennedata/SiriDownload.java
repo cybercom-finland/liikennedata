fe44d6b19695f7c581948952a515f7d37f69237a
unknown <lapel1@virt81-lapel1.global.ad>
2014-07-15 11:24:51 +0300

    Added Siri download, and some packages for hadoop use

---
+++ src/liikennedata/SiriDownload.java
@@ -0,0 +1,114 @@
+package liikennedata;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.*;
+
+import liikennedata.SiriObjects.SiriRoot;
+
+import com.google.gson.Gson;
+
+public class SiriDownload {
+	
+	private final String filePath = "./";
+	private final String outputFileName = "siridata.txt";
+	private final String controlFileName = "control.txt";
+	private final String siriUrl = "http://data.itsfactory.fi/siriaccess/vm/json";
+	private final int sleepTimeMs = 300;
+	
+	private final ExecutorService pool = Executors.newFixedThreadPool(10);
+	
+	public void download() throws InterruptedException, MalformedURLException, IOException, ExecutionException {
+		
+		Number previousTimeStamp = null;
+		Number stamp = null;		
+		
+		File f = new File(filePath + controlFileName);
+		if (!f.exists()) {
+			System.err.println("No control file found, aborting");
+			return;
+		}
+		
+		for (int a = 0; a < 100; a++) {
+			if (shouldAbort()) {
+				System.out.println("Control set to inactive, stopping download");
+				break;
+			}
+			
+			final Future<String> contentsFuture = startDownloading(new URL(siriUrl));
+			final String contents = contentsFuture.get();
+			
+			Gson gson = new Gson();
+			SiriRoot siri = gson.fromJson(contents, SiriRoot.class);
+			stamp = siri.getSiri().getServiceDelivery().getResponseTimestamp();
+				
+			// Only write data if the timestamp has changed
+			if (previousTimeStamp == null || stamp.doubleValue() != previousTimeStamp.doubleValue()) {
+
+				try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filePath + outputFileName, true)))) {
+				    out.print(contents);
+				}catch (IOException e) {
+				    
+				}
+				
+				previousTimeStamp = stamp;
+				Thread.sleep(sleepTimeMs);
+			}
+		}
+	}
+
+	/**
+	 * Checks whether we should abort the download.
+	 * The download should be aborted if the control file has its value set to 0
+	 * @return
+	 * @throws IOException
+	 */
+	private Boolean shouldAbort() throws IOException {
+		List<String> lines = Files.readAllLines(Paths.get(filePath + controlFileName));
+		for (int b = 0; b < lines.size(); b++) {
+			if (!lines.get(b).startsWith("#")) { // # lines are comments
+				if (lines.get(b).contains("0")) { // If line contains 0, then we should abort
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Starts downloading contents of the URL.
+	 * Adapted from http://www.nurkiewicz.com/2013/02/javautilconcurrentfuture-basics.html
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
+	private Future<String> startDownloading(final URL url) throws IOException {
+	    return pool.submit(new Callable<String>() {
+	        @Override
+	        public String call() throws Exception {
+	            try (InputStream input = url.openStream()) {
+	                return convertStreamToString(input);
+	            }
+	        }
+	    });
+	}
+	
+	/**
+	 * Converts an input stream into a string
+	 * Adapted from http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+	 * @param is
+	 * @return
+	 * @throws IOException 
+	 */
+	private String convertStreamToString(java.io.InputStream is) throws IOException {
+	    java.util.Scanner s = new java.util.Scanner(is, "UTF-8").useDelimiter("\\A");
+	    String ret = s.hasNext() ? s.next() : "";
+	    s.close();
+	    is.close();
+	    return ret;
+	}
+}
--- src/liikennedata/Test.java
+++ src/liikennedata/Test.java
@@ -1,10 +1,27 @@
 package liikennedata;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.security.PrivilegedExceptionAction;
 import java.util.concurrent.*;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobID;
+import org.apache.hadoop.mapred.JobStatus;
+import org.apache.hadoop.security.UserGroupInformation;
 
 import liikennedata.SiriObjects.*;
 
@@ -12,48 +29,98 @@ import com.google.gson.Gson;
 
 public class Test {
 
-	public static void main(String[] args) throws MalformedURLException, IOException, InterruptedException, ExecutionException  {
+	public static void main(String[] args) throws MalformedURLException, IOException, InterruptedException, ExecutionException, URISyntaxException  {
 
-		final Future<String> contentsFuture = startDownloading(new URL("http://data.itsfactory.fi/siriaccess/vm/json"));
-		final String contents = contentsFuture.get();
+		SiriDownload dl = new SiriDownload();
+		dl.download();
 		
-		Gson gson = new Gson();
-		SiriRoot obj = gson.fromJson(contents, SiriRoot.class);
-		System.out.println("END");
-	}
+		/*
+		String uri = "hdfs://10.33.24.20:8042/";
 
-	private final static ExecutorService pool = Executors.newFixedThreadPool(10);
 
-	/**
-	 * Starts downloading contents of the URL.
-	 * Adapted from http://www.nurkiewicz.com/2013/02/javautilconcurrentfuture-basics.html
-	 * @param url
-	 * @return
-	 * @throws IOException
-	 */
-	public static Future<String> startDownloading(final URL url) throws IOException {
-	    return pool.submit(new Callable<String>() {
-	        @Override
-	        public String call() throws Exception {
-	            try (InputStream input = url.openStream()) {
-	                return convertStreamToString(input);
-	            }
-	        }
-	    });
-	}
+	    System.out.println( "uri: " + uri );  
+	    System.out.println("path: " + getClasspathString());
+	    Configuration conf = new Configuration();
 
-	/**
-	 * Converts an input stream into a string
-	 * Adapted from http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
-	 * @param is
-	 * @return
-	 */
-	static String convertStreamToString(java.io.InputStream is) {
-	    java.util.Scanner s = new java.util.Scanner(is, "UTF-8").useDelimiter("\\A");
-	    String ret = s.hasNext() ? s.next() : "";
-	    s.close();
-	    return ret;
+	    FileSystem fs = FileSystem.get( URI.create( uri ), conf );
+	    fs.printStatistics();
+	    System.out.println("over");
+	    */
+
+		
+		/*
+		UserGroupInformation ugi = UserGroupInformation
+				.createRemoteUser("hduser");
+
+		try {
+
+			ugi.doAs(new PrivilegedExceptionAction<Void>() {
+
+				public Void run() throws Exception {
+
+					Configuration conf = new Configuration();
+					// fs.default.name should match the corresponding value
+					// in your core-site.xml in hadoop cluster
+					conf.set("fs.defaultFS", "hdfs://10.33.24.20:9000");
+					conf.set("hadoop.job.ugi", "hduser");
+					// in case you are running mapreduce job , need to set
+					// 'mapred.job.tracker' as you did
+					conf.set("mapred.job.tracker", "10.33.24.20:8042");
+
+					System.out.println("got configuration : " + conf);
+					InetSocketAddress jobtracker = new InetSocketAddress(
+							"10.33.24.20", 9005);
+					JobClient jobClient = new JobClient(jobtracker, conf);
+					JobStatus[] jobs = jobClient.jobsToComplete();
+
+					for (int i = 0; i < jobs.length; i++) {
+						JobStatus js = jobs[i];
+						if (js.getRunState() == JobStatus.RUNNING) {
+							JobID jobId = js.getJobID();
+							System.out.println(jobId);
+						}
+					}
+
+					return null;
+				}
+			});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		*/
+		
+		/*
+		Configuration conf = new Configuration();
+	    FileSystem fileSystem = FileSystem.get(new URI("hdfs://10.33.24.20:9000"),conf);
+	    System.out.println("Hello");
+	    
+	    if(fileSystem instanceof DistributedFileSystem) {
+	      System.out.println("HDFS is the underlying filesystem");
+	    }
+	    else {
+	      System.out.println("Other type of file system "+fileSystem.getClass()));*/
 	}
+	
+	public static String getClasspathString() {
+	     StringBuffer classpath = new StringBuffer();
+	     ClassLoader applicationClassLoader = Test.class.getClassLoader();
+	     if (applicationClassLoader == null) {
+	         applicationClassLoader = ClassLoader.getSystemClassLoader();
+	     }
+	     URL[] urls = ((URLClassLoader)applicationClassLoader).getURLs();
+	      for(int i=0; i < urls.length; i++) {
+	          classpath.append(urls[i].getFile()).append("\r\n");
+	      }    
+	     
+	      return classpath.toString();
+	  }
+
+	
+
+	
+
+	
 }
 
 
